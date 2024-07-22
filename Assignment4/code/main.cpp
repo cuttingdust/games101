4b1702ac99c8fdf6bc2cc1f82478dbab0bb58e4f
cuttingDust_47 <18862963156@163.com>
2024-07-22 16:48:57 +0800

    games101 bezier second-start

--- Assignment4/code/main.cpp
+++ Assignment4/code/main.cpp
@@ -30,17 +30,39 @@ void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
     }
 }
 
+cv::Point2f lerp_v2f(const cv::Point2f &a, const cv::Point2f &b, float t)
+{
+    return ( 1 - t) * a + t * b;
+}
+
 cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
 {
     // TODO: Implement de Casteljau's algorithm
-    return cv::Point2f();
+  if (control_points.size() == 1)
+  {
+    return control_points[0];
+  }
 
+  std::vector<cv::Point2f> lerp_points;
+  for (size_t i = 1; i < control_points.size(); ++i) {
+    lerp_points.push_back(lerp_v2f(control_points[i - 1], control_points[i], t));
+  }
+
+    return recursive_bezier(lerp_points, t);
 }
 
 void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
 {
     // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
     // recursive Bezier algorithm.
+
+  for (double t = 0.0f; t <= 1.0 ; t+= 0.001) {
+    /// 对于 每一个t 求他对应的贝塞尔曲线的点 他是通过最基本的公式 (1 - t) * a + t * b 来递归得的
+    /// 实际上也可以直接用 n 阶展开式 直接求的 就像 naive_bezier 函数一样
+    auto point = recursive_bezier(control_points, t);
+
+    window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+  }
 
 }
 
@@ -62,8 +84,8 @@ int main()
 
         if (control_points.size() == 4) 
         {
-            naive_bezier(control_points, window);
-            //   bezier(control_points, window);
+            // naive_bezier(control_points, window);
+            bezier(control_points, window);
 
             cv::imshow("Bezier Curve", window);
             cv::imwrite("my_bezier_curve.png", window);
